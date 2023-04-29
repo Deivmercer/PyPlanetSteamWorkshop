9034bdcc33355ca9db5688f23c7147eb709e2f9a
Davide Costantini <davemercerdave@gmail.com>
2023-04-29 12:35:14 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyplanet-steamworkshop",
+    version="0.1",
+    description="PyPlanet Steam Workshop",
+    author="Davide Costantini",
+    author_email="davemercerdave@gmail.com",
+    url="https://github.com/Deivmercer/PyPlanetSteamWorkshop",
+    packages=find_packages(),
+    install_requires=["pyplanet"]
+)
