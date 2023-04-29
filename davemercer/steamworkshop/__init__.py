f254f20e82e2b81219ec2817de851c1f7a0cb098
Davide Costantini <davemercerdave@gmail.com>
2023-04-29 14:48:12 +0200

    Creating api session

--- davemercer/steamworkshop/__init__.py
+++ davemercer/steamworkshop/__init__.py
@@ -1,13 +1,16 @@
 from pyplanet.apps.config import AppConfig
 
+from api import SteamWorkshopApi
+
 
 class SteamWorkshop(AppConfig):
 
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
+		self.api = SteamWorkshopApi()
 
 	async def on_init(self):
-		await super().on_init()
+		await self.api.create_session()
 
 	async def on_start(self):
 		await super().on_start()
@@ -16,4 +19,4 @@ class SteamWorkshop(AppConfig):
 		await super().on_stop()
 
 	async def on_destroy(self):
-		await super().on_destroy()
+		await self.api.close_session()
